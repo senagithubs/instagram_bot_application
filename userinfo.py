5b03f31e7ea896eafd579157fe951b185e0ca475
senagithubs <160613139+senagithubs@users.noreply.github.com>
2025-03-17 15:04:02 +0300

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -1,15 +1,4 @@
-username = "javel.23_"
-password= "ılovemylife"
-security_code = 28375046
+username = "write your username here"
+password= "write your password here"
+security_code = "write your security_code here"
 
-from dotenv import load_dotenv
-import os
-
-# .env dosyasını yükle
-load_dotenv()
-
-# Çevre değişkenini al
-secret_data = os.getenv("SECRET_KEY")
-
-# Kontrol amaçlı yazdır (Güvenlik için prodüksiyonda bunu yapmayın)
-print(secret_data)
