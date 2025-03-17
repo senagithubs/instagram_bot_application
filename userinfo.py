04c8203f3863710f91cd135cfda72378a0d5b487
senagithubs <senainnkl@outlook.com>
2025-03-17 15:05:54 +0300

    changes_saved

--- userinfo.py
+++ userinfo.py
@@ -2,14 +2,3 @@ username = "javel.23_"
 password= "ılovemylife"
 security_code = 28375046
 
-from dotenv import load_dotenv
-import os
-
-# .env dosyasını yükle
-load_dotenv()
-
-# Çevre değişkenini al
-secret_data = os.getenv("SECRET_KEY")
-
-# Kontrol amaçlı yazdır (Güvenlik için prodüksiyonda bunu yapmayın)
-print(secret_data)
